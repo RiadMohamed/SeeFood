c02eba8db4bfcfdbf4b40f6babda2c583a2d7317
Riad Mohamed <riaduk1@gmail.com>
2020-02-16 20:49:06 +0200

    Initial Commit.
    Add a navigation controller and the Inception V3 CoreML file.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -1,18 +1,12 @@
-//
-//  ViewController.swift
-//  SeeFood
-//
-//  Created by Riad Mohamed on 2/16/20.
-//  Copyright © 2020 Riad Mohamed. All rights reserved.
-//
-
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
