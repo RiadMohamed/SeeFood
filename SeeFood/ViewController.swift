dd38819cb1e7d9992200fd46e55a7ab0c9a31ebf
Riad Mohamed <riaduk1@gmail.com>
2020-02-16 21:44:01 +0200

    L.346
    Add a camera bar button to initiate the ImagePicker Controller, add an IBAction to it "cameraButtonTapped"
    Add an imageView to display the image the user has chosen, add an IBOutlet to it "imageView"
    
    Add user description inside the info.plist file for using the camera and the photo library.
    
    Add imagePicker Controller that will be displayed when the user taps on the camera bar button, return the image the user has chosen from their photo library.
    Cast the userImage as a CIImage and send it to a function called detect.
    
    detect() will send the image to the model and print the results received.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -4,11 +4,51 @@ import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
 
     }
 
-
+    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userImage
+            guard let ciImage = CIImage(image: userImage) else {
+                fatalError("Loading a CIImage from the userImage failed.")
+            }
+            detect(ciImage)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(_ image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Error loading the Inception CoreML model.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Failed to cast the model results.")
+            }
+            print(results)
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch  {
+            print("Failed to perform the handler request. \(error)")
+        }
+    }
+    
 }
 
